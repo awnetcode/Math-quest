4afdcbf4a2aff6140b23066eada5fa09503247fc
awnetcode <155958835+awnetcode@users.noreply.github.com>
2024-10-22 12:12:00 +0200

    buttons-component-and-binary

--- src/components/Binary.jsx
+++ src/components/Binary.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 
 import Draft from "./Draft";
+import ButtonsPanel from "./ButtonsPanel";
+//import { array } from "prop-types";
 
 const Binary = () =>{
     const [loopCounter, setLoopCounter] = useState('');
-   // const [binaryString, setBinaryString] = useState('');
+    const [binaryString, setBinaryString] = useState('');
    const [draftValue, setDraftValue] = useState('');
 
     function binaryLenght(){
@@ -12,12 +14,26 @@ const Binary = () =>{
     }
 
  //funkcja do losowania liczby binarnej   
-/*
-    function binaryValue(){
-        return Math.floor(Math.random());
-    }
-*/
 
+ function binaryValue() {
+    const lenght = binaryLenght(); // Ustal długość ciągu
+    let string = '';
+
+    // Losuj 0 lub 1 tyle razy, ile wynosi długość
+    for (let i = 0; i < loopCounter; i++) {
+        string += Math.floor(Math.random() * 2); // Losuje 0 lub 1
+    }
+    console.log(string);
+    setLoopCounter(lenght); // Aktualizacja długości w stanie
+    setBinaryString(string); // Przechowywanie wyniku w stanie
+}
+
+function binaryToDecimal(){
+        // Zamień ciąg binarny na liczbę dziesiętną
+        const decimalValue = parseInt(binaryString, 2);
+        console.log(binaryString);
+        console.log("Decimal Value:", decimalValue); // Wyświetla wartość dziesiętną
+}
 
     useEffect(() => {
        const lenght = binaryLenght(); //generuje losową wartość
@@ -29,10 +45,11 @@ const Binary = () =>{
         <h1>Zamień liczbę binarną na dziesiętną</h1>
         <div className="task-place">
             <div className="fraction-place">
-            {loopCounter}
+            {binaryString}
             </div>
         </div>
         <Draft draftValue={draftValue} setDraftValue={setDraftValue}/>
+        <ButtonsPanel checkButton = {binaryToDecimal} nextButton = {binaryValue}/>
         </>
     )
 }
---
+++ src/components/ButtonsPanel.jsx
@@ -0,0 +1,15 @@
+import '../styles/buttonsPanel.css';
+
+// eslint-disable-next-line react/prop-types
+const ButtonsPanel = ({checkButton}, {nextButton}) => {
+    return(
+        <>
+             <div className="buttons-panel">
+                <button className='task-button' onClick={checkButton} >Sprawdź</button>
+                <button className='task-button' onClick={nextButton}>NEXT</button>
+            </div>       
+        </>
+    )
+}
+
+export default ButtonsPanel;
